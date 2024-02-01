cd3b99831505c04d9ce484b046a0d03ec47b9dd6
vrajdesai78 <vrajdesai78@gmail.com>
2024-02-02 01:55:49 +0530

    updated UI

--- src/components/ShowPeers.tsx
+++ src/components/ShowPeers.tsx
@@ -26,14 +26,14 @@ const ShowPeers = ({ previewPeers }: previewPeersMetadata) => {
         backgroundColor: 'black',
       }}
     >
-      <span tw='font-bold mt-4'>Huddle01</span>
+      <span tw='font-bold mt-4 text-white'>Huddle01</span>
       <div tw='flex-wrap flex items-center justify-center gap-4 w-full m-6'>
         {peersMetadata.map((peer) => (
           <div tw='flex flex-col text-center gap-1' key={peer.displayName}>
-            <div tw='flex items-center justify-center text-3xl font-semibold w-24 h-24 rounded-full bg-gray-800'>
+            <div tw='flex items-center justify-center text-3xl font-semibold w-24 h-24 rounded-full text-white bg-gray-800'>
               {peer.displayName[0].toUpperCase()}
             </div>
-            <h1>{peer.displayName}</h1>
+            <h1 tw='text-white'>{peer.displayName}</h1>
           </div>
         ))}
       </div>
